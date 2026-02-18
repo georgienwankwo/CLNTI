86a95467e77f9bc220bef3f71bf565412da32535
Georgie Nwankwo <30572471+BuiltbyGit@users.noreply.github.com>
2026-02-18 05:13:24 +0100

    fix: environment variables

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,6 +9,6 @@ export const environment = {
     appId: '1:502388164882:web:35e270a964c86c95900bcb',
     measurementId: 'G-VB0JJVVKZ4',
   },
-  paystackPublicKey: `BwLJTu2vJNKdRE6Eat7g`,
+  paystackPublicKey: `BwLJTu2vJNKdRE6Eat7gs`,
   apiUrl: 'http://localhost:5001/clnt-un/us-central1/api',
 };
